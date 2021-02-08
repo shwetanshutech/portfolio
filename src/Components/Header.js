589ad49cd4828cb69759d545fbb814ab2465fcfa
shwetanshutech <shwetanshu2000@gmail.com>
2021-02-08 20:07:55 +0530

    change1

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -51,7 +51,7 @@ const Header = ({ data }) => {
           </li>
           <li>
             <a className="smoothscroll" href="#testimonials">
-              Testimonials
+              Quotes i believe in
             </a>
           </li>
           <li>
