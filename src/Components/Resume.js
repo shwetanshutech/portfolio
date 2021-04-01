f8ca40b8624ef6fd5ad2744ff2f1982dec0ba3e0
shwetanshutech <shwetanshu2000@gmail.com>
2021-04-01 18:18:08 +0530

    removed skill section

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -27,15 +27,6 @@ const Resume = ({ data }) => {
         </div>
       );
     });
-    var skills = data.skills.map(function (skills) {
-      var className = "bar-expand " + skills.name.toLowerCase();
-      return (
-        <li key={skills.name}>
-          <span style={{ width: skills.level }} className={className}></span>
-          <em>{skills.name}</em>
-        </li>
-      );
-    });
   }
 
   return (
@@ -62,22 +53,6 @@ const Resume = ({ data }) => {
         </div>
 
         <div className="nine columns main-col">{work}</div>
-      </div>
-
-      <div className="row skill">
-        <div className="three columns header-col">
-          <h1>
-            <span>Skills</span>
-          </h1>
-        </div>
-
-        <div className="nine columns main-col">
-          <p>{skillmessage}</p>
-
-          <div className="bars">
-            <ul className="skills">{skills}</ul>
-          </div>
-        </div>
       </div>
     </section>
   );
